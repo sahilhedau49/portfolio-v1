de95c428264357f88e16c8e8f372168f13971eb0
sahilhedau49 <hedausahil49@gmail.com>
2024-05-10 00:11:50 +0530

    Loading Design Started

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Hero from "./Hero";
 import Skills from "./Skills";
 import ProjectSection from "./ProjectSection";
 import CodingProfiles from "./CodingProfiles";
 import Contact from "./Contact";
 import Experience from "./Experience";
+import "../Global.css";
 
 const MainPage = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <>
       <Hero />
@@ -21,6 +31,16 @@ const MainPage = () => {
       <div className="mt-20">
         <Contact />
       </div>
+      {loading && (
+        <div className="select-none loading-page min-h-screen min-w-full bg-gray-900 absolute top-0 left-0 flex justify-center">
+          <p className="fill text-gray-800 font-extrabold text-[16rem] place-content-center">
+            &lt; SH49 /&gt;
+          </p>
+          <p className="absolute top-36 right-24 text-gray-800 font-extrabold text-[16rem] place-content-center">
+            &lt; SH49 /&gt;
+          </p>
+        </div>
+      )}
     </>
   );
 };
